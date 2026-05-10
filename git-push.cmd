773d8731c6aa4d9cf9a6d627c8bd08d35f42abce
CHACCHAN <chacchan.info@gmail.com>
2026-05-10 13:34:00 +0900

    chore: sync framework core from subdir script

--- git-push.cmd
+++ git-push.cmd
@@ -5,8 +5,8 @@ cd /d %~dp0..
 git remote add bot-origin https://github.com/CHACCHAN/cc-discord-framework.git 2>nul
 
 git add .
-git commit -m "chore: auto sync via script"
+git commit -m "chore: sync framework core from subdir script"
 
-git subtree push --prefix=discord-bot bot-origin main
+git subtree push --prefix=discord-bot/core bot-origin main
 
 pause
