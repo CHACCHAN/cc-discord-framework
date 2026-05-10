98357a6730d4ecb3a774353a9e7b8cfd906647aa
CHACCHAN <chacchan.info@gmail.com>
2026-05-10 19:59:44 +0900

    chore: sync bot core from subdir script

--- git-sync.cmd
+++ git-sync.cmd
@@ -1,17 +1,17 @@
 @echo off
-
 chcp 65001 >nul
 
 cd /d %~dp0..
 
 echo.
-echo [1/4] リモートリポジトリ(bot-origin)の設定を確認中...
-git remote add bot-origin https://github.com/CHACCHAN/cc-discord-framework.git 2>nul
+echo [1/4] リモートリポジトリ(bot-origin)の設定を更新中...
+git remote remove bot-origin 2>nul
+git remote add bot-origin https://github.com/CHACCHAN/CC-System.git
 
 echo.
-echo [2/4] ローカルの最新の変更を保存 (Commit) しています...
+echo [2/4] ローカルの最新の変更を保存しています...
 git add .
-git commit -m "chore: sync framework core from subdir script"
+git commit -m "chore: sync bot core from subdir script"
 
 echo.
 echo [3/4] GitHub側の新しい変更を取り込み (Subtree Pull) しています...
